43feda06027cb3f955c96a1492c82aef396592c0
Zharif-1135 <zharif.230170004@mhs.unimal.ac.id>
2025-11-12 15:31:13 +0700

    Fix Folder Structure

--- models/Note.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const NoteSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
-  encryptedText: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Note', NoteSchema);
--- routes/notes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/authMiddleware');
-const { createNote, getNotes } = require('../controllers/notesController');
-
-router.post('/', auth, createNote);
-router.get('/', auth, getNotes);
-
-module.exports = router;
